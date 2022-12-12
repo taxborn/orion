29a35ff26af114639bc99ea55dcd066d1843fbb3
Braxton Fair <hello@taxborn.com>
2022-12-11 22:39:41 -0600

    Add logging info and add `Result` to main

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::path::PathBuf;
+use std::{path::PathBuf, io::Error};
 
 use clap::Parser;
 
@@ -7,23 +7,30 @@ use clap::Parser;
 struct Args {
     /// The path of the file to compile
     #[arg(short, long)]
-    path: Option<PathBuf>
+    file: Option<PathBuf>
 }
 
-fn main() {
+fn main() -> Result<(), Error>{
     let args = Args::parse();
     let mut contents = String::new();
 
     // Check if a path was supplied
-    if let Some(file) = args.path {
+    if let Some(file) = args.file {
         // File was passed
-        contents = std::fs::read_to_string(file).unwrap();
+        contents = std::fs::read_to_string(&file)?;
+        
+        println!("[lexing] {file:?}");
     } else {
         // File was not passed. Eventually I'd like to implement a REPL, however
         // for the time being, I'll just default to the 'examples/main.ori' file
         // to lex/parse/etc..
-        contents = std::fs::read_to_string("examples/main.ori").unwrap();
+        contents = std::fs::read_to_string("examples/main.ori")?;
+        
+        println!("[lexing] \"examples/main.ori\"")
     }
 
-    println!("{contents}");
+    // Ensure we have a file
+    assert!(contents.len() > 0, "Unable to read file");
+
+    Ok(())
 }
